478fd577840db0b4e1b5a145af9d557868bef8c0
Ctreadwell92 <christophertreadwell92@gmail.com>
2026-01-17 21:40:33 -0600

    Added greet function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
-int main() {
+void greet() {
 	// Print some text
 	std::cout << "Hello World\n\n" << std::endl;
 	std::cout << "Welcome\n\n\n" << std::endl;
+}
+
+int main() {
+	
+	greet();
 
 	return 0;
 }
